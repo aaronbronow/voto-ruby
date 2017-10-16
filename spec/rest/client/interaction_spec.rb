6ef50ee9d9bc2aed760880b4a5d8a476f682e715
Krzysztof Witczak <kwitczak@soldevelo.com>
2017-10-16 08:41:45 +0200

    Fixed typo

--- spec/rest/client/interaction_spec.rb
+++ spec/rest/client/interaction_spec.rb
@@ -7,7 +7,7 @@ describe VotoMobile::Interaction do
     stub_voto_request('trees/10/blocks/228/interactions', 'interactions')
 
     data = client.interactions_by_tree_block(10, 228)
-    expect(data).to be_a VotoMobile::InteractionList
+    expect(data).to be_a VotoMobile::InteractionsList
     expect(data.list[0]).to be_a VotoMobile::Interaction
     expect(data.list.length).to eq 5
   end
