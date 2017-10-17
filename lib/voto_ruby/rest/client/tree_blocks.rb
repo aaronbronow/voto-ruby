8adac2e1dbf8f460205a9e8f72bf3ba71371be47
Krzysztof Witczak <kwitczak@soldevelo.com>
2017-10-17 13:02:10 +0200

    Removed unnecessary method from tree blocks

--- lib/voto_ruby/rest/client/tree_blocks.rb
+++ lib/voto_ruby/rest/client/tree_blocks.rb
@@ -4,21 +4,12 @@ module VotoMobile
       list = TreeBlocksList.new self, 'blocks'
       data = get("trees/#{tree_id}/blocks")
       try_paginate(data, list)
-      list.assign_data(data['data']['blocks'], tree_id)
+      list.assign_data(data['data']['blocks'])
       list
     end
   end
 
   class TreeBlocksList < BaseList
     ENTITY = TreeBlock
-
-    def assign_data(list, tree_id)
-      unless list.length > 0
-        @data = []
-        return
-      end
-
-      @data = list.map { |hash| self.class::ENTITY.new(hash.merge(tree_id: tree_id), @client) }
-    end
   end
 end
